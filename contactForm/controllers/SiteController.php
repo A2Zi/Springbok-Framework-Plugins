3e4d64703d87c4650cd7aa0d81509b01787b0f37
Fabien Palomares <f.palomares@a2zi.fr>
2013-10-24 23:42:40 +0200

    Allow customized siteName and DestEmail for projects with multiple domains

--- contactForm/controllers/SiteController.php
+++ contactForm/controllers/SiteController.php
@@ -11,8 +11,8 @@ class SiteController extends AController{
 	static function contact_submit($name,$email,$subject,$content,$captcha){
 		if(!CValidation::hasErrors() && CCaptcha::check()){
 			$mailer=CMail::get();
-			$mailer->AddAddress(Config::$adminEmail);
-			$mailer->Subject='['.Config::$projectName.'] Contact de: '.$email;
+			$mailer->AddAddress(Config::$contactFormDestEmail[Springbok::$scriptname]);
+			$mailer->Subject='['.Config::$contactFormSiteName[Springbok::$scriptname].'] Contact de: '.$email;
 			$mailer->Body='<p><b>Sujet : '.$subject.'</b></p><p>'.h($content).'</p>';
 			$mailer->AddReplyTo($email);
 			$mailer->Send();
