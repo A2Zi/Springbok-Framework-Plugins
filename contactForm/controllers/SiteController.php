a4f6e78689d57065b522b1a5f03dc7c8bc88733f
Fabien Palomares <f.palomares@a2zi.fr>
2013-10-25 11:43:43 +0200

    contactForm legacy

--- contactForm/controllers/SiteController.php
+++ contactForm/controllers/SiteController.php
@@ -11,8 +11,8 @@ class SiteController extends AController{
 	static function contact_submit($name,$email,$subject,$content,$captcha){
 		if(!CValidation::hasErrors() && CCaptcha::check()){
 			$mailer=CMail::get();
-			$mailer->AddAddress(Config::$contactFormDestEmail[Springbok::$scriptname]);
-			$mailer->Subject='['.Config::$contactFormSiteName[Springbok::$scriptname].'] Contact de: '.$email;
+			$mailer->AddAddress(isset(Config::$contactFormDestEmail[Springbok::$scriptname])?Config::$contactFormDestEmail[Springbok::$scriptname]:Config::$adminEmail);
+			$mailer->Subject='['.isset(Config::$contactFormSiteName[Springbok::$scriptname])?Config::$contactFormSiteName[Springbok::$scriptname]:Config::$projectName.'] Contact de: '.$email;
 			$mailer->Body='<p><b>Sujet : '.$subject.'</b></p><p>'.h($content).'</p>';
 			$mailer->AddReplyTo($email);
 			$mailer->Send();
